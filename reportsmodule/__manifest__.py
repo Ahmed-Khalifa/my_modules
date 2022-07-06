1d0e600968b8bcdd395b94f21a4ae9406ae839d6
Ahmed A. Khalifa <ahmedKhalifa090@gmail.com>
2022-07-06 13:36:58 +0300

    Update __manifest__.py

--- reportsmodule/__manifest__.py
+++ reportsmodule/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Reports Module",
+    'name': "reports_module",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','sale_management],
+    'depends': ['base','sale','sale_management'],
 
     # always loaded
     'data': [
