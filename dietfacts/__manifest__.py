f000d04f6d45c0183eca2fcf37f2956e0cbe9682
Ahmed-Khalifa <ahmedkhalifa090@gmail.com>
2023-01-22 17:17:40 +0300

    upload

---
+++ dietfacts/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Diet Facts',
+    'version': '0.1',
+    'description': 'Nutritional food facts',
+    'summary': 'Diet facts summary',
+    'author': 'Ahmed Khalifa',
+    'website': 'aliaict.com',
+    'license': 'LGPL-3',
+    # 'category': '',
+    'depends': [
+        'sale','base'
+    ],
+    'auto_install': False,
+    'application': False,
+    'installable':True,
+}
