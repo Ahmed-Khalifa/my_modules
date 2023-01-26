c5579c28728c79bb8810ee8200fa4d37d2d78018
Ahmed-Khalifa <ahmedkhalifa090@gmail.com>
2023-01-26 16:30:01 +0300

    Delete reports_module for it is a test.

---
+++ hospital_management/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Hospital Management',
+    'version': '0.0.1', # Versioning model major.minor.patch-fix
+    'description': 'Manage Hospital patients data',
+    'summary': 'Manage Hospital patients data',
+    'author': 'Ahmed Khalifa',
+    'website': 'https://micloud.nohost.me',
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/menu.xml'
+    ],
+    # 'demo': [
+    #     ''
+    # ],
+    'auto_install': False,
+    'application': True,
+    'sequence':-98,
+    # 'assets': {
+        
+    # }
+}
---
+++ hospital_management/models/patient/patient.py
@@ -0,0 +1,11 @@
+from odoo import models,fields
+
+class HospitalPatient (models.Model):
+    _name="hospital.patient"
+    _description="Hospital Patient"
+    
+    age = fields.Integer("Age")
+    gender = fields.Selection([('male', 'Male'),('female', 'Female')])
+    name = fields.Char("Name")
+    
+    
